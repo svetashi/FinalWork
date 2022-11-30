037742af1d436fbd73b06788fc11c1c3d1b157b4
Sveta Shi <svetashiblieva@gmail.com>
2022-11-30 19:38:13 +0300

    добавили цикл for по которому будем двигаться пока он не закончится

--- TASK/Program.cs
+++ TASK/Program.cs
@@ -2,3 +2,8 @@
 Console.WriteLine("Привет, введи любые значения через запятую: ");
 string any = Console.ReadLine();
 string [] array = any.Split(',');
+
+for (int i = 0; i < array.Length; i++)
+{
+    
+}
